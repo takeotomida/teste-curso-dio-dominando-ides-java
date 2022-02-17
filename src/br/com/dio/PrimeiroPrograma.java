9081a70f5dc684833ec73f8def7370045e005367
takeo <vttomida@gmail.com>
2022-02-17 10:48:55 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/br/com/dio/model/gato.java
@@ -0,0 +1,57 @@
+package br.com.dio.model;
+
+public class gato {
+	
+	private string nome;
+	private string cor;
+	private integer idade;
+	
+	public gato() {}
+
+	public gato(string nome, string cor, integer idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+	public string getNome() {
+		return nome;
+	}
+
+	public void setNome(string nome) {
+		this.nome = nome;
+	}
+
+	public string getCor() {
+		return cor;
+	}
+
+	public void setCor(string cor) {
+		this.cor = cor;
+	}
+
+	public integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(integer idade) {
+		this.idade = idade;
+	}
+	
+	/* public gato (string nome, string cor, integer idade) {
+	 
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}*/
+}
+	
+
+	
+	
+
+
+
+
+
+	
